e20c09f3bdc3110aa66bc049d8af0bea24d68eac
Jackie Van Slycke <Jackie Van Slycke>
2015-12-08 10:24:20 -0500

    This isn't completed yet

---
+++ babbagescode.py
@@ -0,0 +1,19 @@
+Python 2.7.10 |Anaconda 2.3.0 (64-bit)| (default, May 28 2015, 16:44:52) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def statistics (grades):
+	print min(grades)
+	print max(grades)
+	print 
+
+
+	
+>>> def statistics (grades):
+	print min(grades)
+	print max(grades)
+	print (sum(grades)/float(len(grades)))
+	import numpy as np
+	np.mean(grades)
+	np.std(grades)
+
+
+
